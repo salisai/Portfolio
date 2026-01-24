705a0c5b6e5e1a88e1846fc90ebdf59390443622
salisai <sayedalishahshah5@gmail.com>
2026-01-24 14:56:33 +0500

    feat(OG): reconfigure opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,38 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'Developer Portfolio | Full-Stack & AI',
-  description: 'Full-Stack Developer and AI Engineer portfolio showcasing projects and expertise,showcase articles on how AI, backend systems work.',
+  description: 'Full-Stack Developer and AI Engineer portfolio showcasing projects and expertise, showcase articles on how AI, backend systems work.',
+  metadataBase: new URL('https://sayedalishah.com'), // Helps with absolute URLs
+
+  openGraph: {
+    title: 'Sayed Ali Shah - Full-Stack & AI Developer',
+    description: 'Personal portfolio showcasing full-stack, AI projects, experience and skills.',
+    url: 'https://sayedalishah.com',
+    siteName: 'Portfolio',
+    images: [
+      {
+        url: '/opengraph-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Sayed Ali Shah Portfolio Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image', 
+    title: 'Portfolio',
+    description: 'Full-Stack & AI Developer • Projects • Experience • Portfolio',
+    images: ['/twitter-image.jpg'], 
+  },
+
   icons: {
     icon:
       'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="white" d="M27,2H5c-1.654,0-3,1.346-3,3V27c0,1.654,1.346,3,3,3H27c1.654,0,3-1.346,3-3V5c0-1.654-1.346-3-3-3Zm1,25c0,.551-.448,1-1,1H5c-.552,0-1-.449-1-1V5c0-.551,.448-1,1-1H27c.552,0,1,.449,1,1V27ZM12.707,15.293c.391,.391,.391,1.023,0,1.414l-4,4c-.195,.195-.451,.293-.707,.293s-.512-.098-.707-.293c-.391-.391-.391-1.023,0-1.414l3.293-3.293-3.293-3.293c-.391-.391-.391-1.023,0-1.414s1.023-.391,1.414,0l4,4Zm12.293,4.707c0,.552-.447,1-1,1H15c-.553,0-1-.448-1-1s.447-1,1-1h9c.553,0,1,.448,1,1Z"/></svg>',
   },
-  twitter: {
-    card: 'summary_large_image',
-    images: '/opengraph-image'
-  }
-}
-
+};
 export default function RootLayout({
   children,
 }: Readonly<{
